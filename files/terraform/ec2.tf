d837ad27218e9d9eb952348164d4d6d13b51cd79
hussain-mahammad08 <kattubadi.mahammadhussain@gmail.com>
2025-12-14 22:35:26 +0530

    Restructure repo: Jenkins + Terraform + Ansible deploy pipeline

---
+++ files/terraform/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "petcart_sg" {
+  name = "petcart-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "petcart" {
+  ami                    = "ami-0e001c9271cf7f3b9" # Amazon Linux 2023 us-east-1
+  instance_type          = "t2.micro"
+  key_name               = "hussaincloud"
+  vpc_security_group_ids = [aws_security_group.petcart_sg.id]
+
+  tags = {
+    Name = "petcart-shop"
+  }
+}
