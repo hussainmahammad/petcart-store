53f17cd5a86edad887ccdaa2c343621dc6b9f82a
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-01-27 15:15:36 +0530

    Update outputs.tf

--- files/terraform/ec2/outputs.tf
+++ files/terraform/ec2/outputs.tf
@@ -1,1 +1,15 @@
+output "public_ip" {
+  value = aws_instance.petcart.public_ip
+}
 
+output "instance_id" {
+  value = aws_instance.petcart.id
+}
+
+output "nginx_access_log_group" {
+  value = "/petcart/nginx/access"
+}
+
+output "nginx_error_log_group" {
+  value = "/petcart/nginx/error"
+}
