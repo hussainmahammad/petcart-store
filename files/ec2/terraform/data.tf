84b64c83559ceb9e0644148d7c3c9217128024cd
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-04 11:14:04 +0530

    Update data.tf

--- files/ec2/terraform/data.tf
+++ files/ec2/terraform/data.tf
@@ -1,1 +1,24 @@
+# ----------------------------
+# Discover default VPC
+# ----------------------------
+data "aws_vpc" "default" {
+  default = true
+}
 
+# ----------------------------
+# Discover public subnets
+# ----------------------------
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# ----------------------------
+# Fetch subnet details
+# ----------------------------
+data "aws_subnet" "public" {
+  for_each = toset(data.aws_subnets.public.ids)
+  id       = each.value
+}
