0803ad21f187dbf824c393b05c6a09cbf30412e4
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-04-22 08:17:09 +0530

    Update backend.tf

--- files/ec2/terraform/backend.tf
+++ files/ec2/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "hussainmahammad.online-tfstates"
+    bucket         = "hussainmahammad.online-tfstates1"
     key            = "petcart/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "petcart-terraform-locks"
