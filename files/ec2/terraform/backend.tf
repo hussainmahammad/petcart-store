4ee33a9b8ce266330d998f8b572ae9322d0b1ca0
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-05-29 14:26:05 +0530

    Update backend.tf

--- files/ec2/terraform/backend.tf
+++ files/ec2/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "hussainmahammad.online-tfstates1"
+    bucket         = "tfstates-hussainmahammad.online"
     key            = "petcart/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
